9ac30b1d42607c4da1c90aed91aecce91f16fe75
Adetoyese Adeniran <Adeniranadetoyese25@gmail.com>
2025-10-27 15:44:17 -0700

    all

--- src/pages/design.jsx
+++ src/pages/design.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function design() {
+export default function Design() {
   return (
-    <div>
-      hi
+    <div className="p-6 text-center">
+      <h2 className="text-2xl font-semibold mb-4">Doctor Appointment Website</h2>
+      <img
+        src="public/Appointment App.jpg"
+        alt="Doctor Appointment Website Design"
+        className="rounded-2xl shadow-lg mx-auto max-w-full h-auto"
+      />
     </div>
-  )
+  );
 }
